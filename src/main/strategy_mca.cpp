5e80e384964e3f963aed2104dee219407347c538
Mcx <milan.cihak@brouzdej.net>
2022-06-09 15:37:41 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -49,7 +49,15 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
     double availableCurrency = std::max(0.0, st.currency);
     double cfgInitBet = cfg.initBet;
 	double size = 0;
-    double minAboveEnterPerc = cfg.minAboveEnter / 100;
+    double minAboveEnterPerc = (std::isnan(cfg.minAboveEnter) || cfg.minAboveEnter <= 0) ? 0 : cfg.minAboveEnter / 100;
+
+    // sign = (std::isnan(cfg.minAboveEnter) || cfg.minAboveEnter <= 0) ? 0 : cfg.minAboveEnter / 100;
+    // is equivalent to
+    // if (std::isnan(cfg.minAboveEnter) || cfg.minAboveEnter <= 0) {
+    //     sign = 0;
+    // } else {
+    //     sign = cfg.minAboveEnter / 100;
+    // }
 
     bool alert = true;
 
