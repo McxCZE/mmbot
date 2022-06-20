5d897930896baa26f61c921b7ace1590ab7cb82d
Mcx <milan.cihak@brouzdej.net>
2022-06-20 18:11:03 +0200

    Update strategy.cpp

--- src/main/strategy.cpp
+++ src/main/strategy.cpp
@@ -71,7 +71,7 @@ Strategy Strategy::create_base(std::string_view id, json::Value config) {
 		cfg.buyStrength = config["buyStrength"].getNumber();
 		cfg.sellStrength = config["sellStrength"].getNumber();
 		cfg.initBet = config["initBet"].getNumber();
-		cfg.minAboveEnter = config["minAboveEnter"].getNumber();
+		cfg.minPnl = config["minPnl"].getNumber();
 		return Strategy(new Strategy_Mca(cfg));
 	} else if (id == Strategy_HalfHalf::id) {
 		Strategy_HalfHalf::Config cfg;
