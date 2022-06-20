5bd26d88cf4d4deadab388978baea4a13ad7e287
Mcx <milan.cihak@brouzdej.net>
2022-06-20 19:37:57 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -60,23 +60,21 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
     
 	if (enterPrice == 0 || effectiveAssets < minSize) { // effectiveAssets < ((cfgInitBet/ 100) * st.budget) / price
         size = (initialBetSize > minSize && dir > 0) ? initialBetSize : (dir > 0) ? minSize : 0;
-        size = (price > st.last_price) ? 0 : size;
-		size = (price < st.last_price) ? 0 : size;
+        // size = (price > st.last_price) ? 0 : size;
 
-		// if (price > st.last_price) {
-		// 	// Move last price up with alert, unless downtrend mode is enabled
-		// 	alert = !cfg.downtrend;
-		// 	size = 0;
-		// } else if (st.sentiment > 0 && !cfg.downtrend) {
-		// 	// Move last price up or down with alert due to uptrend sentiment
-		// 	alert = true;
-		// 	size = 0;
-		// } else {
-		// 	size = (availableCurrency * cfg.initial_bet_perc_of_budget) / price;
-		// 	if (st.alerts > 0) {
-		// 		size *= 0.5;
-		// 	}
-		// }
+		if (price > st.last_price) {
+			// Move last price up with alert, unless downtrend mode is enabled
+			alert = true;
+			size = 0;
+		} else if (st.sentiment > 0) {
+			// Move last price up or down with alert due to uptrend sentiment
+			alert = true;
+			size = 0;
+		} else {
+			if (st.alerts > 0) {
+				size *= 0.5;
+			}
+		}
 
         // size = (st.sentiment > 0) ? 0 : size; // Nedari se mi to rozfungovat.
         // size = (st.alerts > 0) ? ((size / 2) < minSize ? minSize : size / 2) : size; //puvodne st.alerts
