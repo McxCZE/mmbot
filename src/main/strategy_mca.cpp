fa4a23a5ac98d7f59773c2121e96dd0afe9a8d0c
Mcx <milan.cihak@brouzdej.net>
2022-06-20 17:13:18 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -86,7 +86,7 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
         
         if (dir > 0 && enterPrice > price) {
             size = std::max(0.0, std::min(assetsToHoldWhenBuying - effectiveAssets, availableCurrency / price));
-            // size = size / ((st.sentiment < 0) ? 1 + std::abs(st.sentiment) : 1);
+            size = size / ((st.sentiment < -4) ? 1 + std::abs(st.sentiment) : 1);
             size = size < minSize ? 0 : size;
         }
 
