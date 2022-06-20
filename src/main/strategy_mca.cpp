c9f61e2f25d7dd7a4ae20edb4f57cb7effc68bf3
Mcx <milan.cihak@brouzdej.net>
2022-06-20 08:11:09 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -53,7 +53,7 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
     double cfgSellStrength = (cfg.sellStrength <= 0.0 || std::isnan(cfg.sellStrength)) ? 0 : cfg.sellStrength;
     double cfgBuyStrength = (cfg.buyStrength <= 0.0 || std::isnan(cfg.buyStrength)) ? 0 : cfg.buyStrength;
     double minAboveEnterPerc = (cfg.minAboveEnter <= 0.0) ? 0 : cfg.minAboveEnter / 100;
-    // bool downtrend = (minAboveEnterPerc == 0.0) ? false : true;
+    bool downtrend = (minAboveEnterPerc == 0.0) ? false : true;
 
 	double size = 0;
     double pnl = (effectiveAssets * price) - (effectiveAssets * enterPrice);
@@ -62,8 +62,8 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
     
 	if (enterPrice == 0 || effectiveAssets < minSize) { // effectiveAssets < ((cfgInitBet/ 100) * st.budget) / price
         size = (initialBetSize > minSize && dir > 0.0) ? initialBetSize : minSize;
-        // size = (st.sentiment > 0) ? 0 : size;
-        // size = (st.alerts > 0) ? ((size / 2) < minSize ? minSize : size / 2) : size;
+        size = (st.sentiment > 0) ? 0 : size; // Nedari se mi to rozfungovat.
+        size = (st.alerts > 0) ? ((size / 2) < minSize ? minSize : size / 2) : size;
 
         // if (price > st.last_price) {alert = !downtrend; size = 0;}
         // else if (st.sentiment > 0 && !downtrend) {alert = true; size = 0;} 
