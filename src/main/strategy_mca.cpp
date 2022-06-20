27d69050b3f984b6c894a59ce9cf6970d5bc7cf2
Mcx <milan.cihak@brouzdej.net>
2022-06-20 20:34:38 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -106,7 +106,7 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
 		size = size * dir;
 
         //Do not sell if in Loss.
-        if (pnlPercentage < 0 + minPnlPercentage && dir < 0) { size = 0; }
+        // if (pnlPercentage < 0 + minPnlPercentage && dir < 0) { size = 0; }
     }
 
     return {size, alert};
