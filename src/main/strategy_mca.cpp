4dc65fd17c3e016ee1903d6cea5208d10cfd09b0
Mcx <milan.cihak@brouzdej.net>
2022-07-13 18:43:54 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -54,11 +54,11 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
     double cfgBuyStrength = (cfg.buyStrength <= 0.0 || std::isnan(cfg.buyStrength)) ? 0 : cfg.buyStrength;
     double minPnlPercentage = (cfg.minPnl <= 0.0) ? 0 : cfg.minPnl / 100;
     double pnlPercentage = ((price / enterPrice) - 1);
-	double stoploss = (cfg.stoploss <= 0.0) ? 0 : cfg.stoploss / 100;
+	double stoploss = (cfg.stoploss >= 0.0) ? 0 : cfg.stoploss / 100; // Blbost. musi to byt negativni.
 
 	double size = 0;
     bool alert = true;
-    
+
 	if (enterPrice == 0 || effectiveAssets < minSize) {
         size = (initialBetSize > minSize) ? initialBetSize : minSize + (minSize * 0.5);
 		size = (dir > 0) ? size : 0;
