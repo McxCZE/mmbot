eb810f86bd4d8a5d0d79d3db24f725b4b75385fc
Mcx <milan.cihak@brouzdej.net>
2022-07-13 09:08:37 +0200

    Update strategy_mca.cpp

--- src/main/strategy_mca.cpp
+++ src/main/strategy_mca.cpp
@@ -76,19 +76,19 @@ std::pair<double, bool> Strategy_Mca::calculateSize(double price, double assets,
         double assetsToHoldWhenBuying = ((budget * buyStrength) / price); //enterPrice
         double assetsToHoldWhenSelling = (cfgSellStrength <= 0) ? effectiveAssets : (budget * sellStrength) / price; //Never Sell
         
-        if (dir > 0) { //&& enterPrice > price
+        if (dir > 0 && dir < 0) { //&& enterPrice > price
             size = std::max(0.0, std::min(assetsToHoldWhenBuying - effectiveAssets, availableCurrency / price));
             size = size < minSize ? 0 : size;
 
 			if (size == 0) {alert = false; return {size, alert};}
         }
 
-        if (dir < 0) { //&& enterPrice < price
-            size = std::max(0.0, std::min(std::abs(assetsToHoldWhenSelling - effectiveAssets), effectiveAssets));
-            size = size < minSize ? 0 : size;
-            size = cfgSellStrength >= 1 ? effectiveAssets : size; //Sell All
-            size = size > 0 ? size * -1 : 0;
-        }
+        // if (dir < 0) { //&& enterPrice < price
+        //     size = std::max(0.0, std::min(std::abs(assetsToHoldWhenSelling - effectiveAssets), effectiveAssets));
+        //     size = size < minSize ? 0 : size;
+        //     size = cfgSellStrength >= 1 ? effectiveAssets : size; //Sell All
+        //     size = size > 0 ? size * -1 : 0;
+        // }
     }
 
     return {size, alert};
